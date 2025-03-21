23bfe7d6e38a3bbbe286df20d13a84b04a03978f
Soumya Jaiswal <130769536+nios-x@users.noreply.github.com>
2025-03-21 08:53:36 +0530

    Delete app/api/signup/[stage] directory

--- app/api/signup/[stage]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(request, { params }:{params:Promise<{stage:string}>}) { 
-  const { stage } = await params;
-  console.log(stage);
-  return NextResponse.json({})
-}
