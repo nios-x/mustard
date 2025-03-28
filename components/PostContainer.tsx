948d0ba5fa08b5483b06d66153c9baf2b37e26bd
nios-x <devnios7@gmail.com>
2025-03-29 00:20:44 +0530

    0.3.4 Frontend Posts Integration

--- components/PostContainer.tsx
+++ components/PostContainer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PostContainer(props:any) {
   return (
     <div className='p-2'>
-        {props.posts.map((e)=>(<>
+        {props.posts.map((e:any)=>(<>
             <div key={e.createdAt} className='text-sm p-3 rounded-lg  border'>
                 <div>{e.user.name} - {e.createdAt}</div>
                 <div>{e.user.username}</div>
