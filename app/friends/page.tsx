e8c4c1431ebe1d57362c54247bdee6020941a234
nios-x <devnios7@gmail.com>
2025-03-30 20:07:01 +0530

    0.3.7 More Proper

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 
 export default function () {
+  useEffect(()=>{
+    (()=>{
+      
+    })()
+  },[])
   return (
     <div>
       <div>
         Friends
       </div>
+      <div>
 
+      </div>
     </div>
   )
 }
