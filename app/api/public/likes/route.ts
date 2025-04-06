d858557a17d3052947c5f286d5fb422317b1844d
nios-x <devnios7@gmail.com>
2025-04-06 22:06:34 +0530

    0.5.4 Added Likes Share Comment Design

--- app/api/public/likes/route.ts
+++ app/api/public/likes/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-export async function POST(request: Request):any{
+export async function POST(request: Request):Promise<any>{
     const data = await request.json()
-    
+    return NextResponse.json({})
 
 }
